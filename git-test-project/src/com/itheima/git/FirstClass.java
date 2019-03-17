a2bbefc2ba2fa86bd2b20eec7a23d1ae17457073
nicko <530473527@qq.com>
2019-03-17 17:20:23 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("我要成为java高级工程师");
+        System.out.println("我要成为java架构工程师");
+        System.out.println("我要站在java的顶端");
+    }
+}
