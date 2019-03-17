c56f13401f224ae211c8173403b7bc97ecacae40
nicko <530473527@qq.com>
2019-03-17 17:34:52 +0800

    修改firstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -5,9 +5,11 @@ public class FirstClass {
         System.out.println("我要成为java高级工程师");
         System.out.println("我要成为java架构工程师");
         System.out.println("我要站在java的顶端");
+        System.out.println("你一定可以");
     }
     public void test(){
         int i=0;
         i+=10;
+        System.out.println(i);
     }
 }
